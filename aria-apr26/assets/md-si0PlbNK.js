1e348e94c8dda7c4675ac218430be876a888826f
disconcision <me@andrewblinn.com>
2026-04-16 11:42:20 +0100

    aria slideshow

---
+++ aria-apr26/assets/md-si0PlbNK.js
@@ -0,0 +1,6 @@
+import{o as i,b as d,w as n,g as e,j as s,v as l,x as m,T as o}from"./modules/vue-BgBTcbuE.js";import{I as p}from"./slidev/default-BWqE2yG6.js";import{u,f as c}from"./slidev/context-CF7M-MPt.js";import"./index-Bzb_qAVu.js";import"./modules/shiki-h8s-_NRu.js";const C={__name:"slides.md__slidev_13",setup(g){const{$clicksContext:a,$frontmatter:r}=u();return a.setup(),(f,t)=>(i(),d(p,l(m(o(c)(o(r),12))),{default:n(()=>[t[0]||(t[0]=e("h1",null,"Computational Glue on a Computational Canvas",-1)),t[1]||(t[1]=e("div",{class:"text-sm mb-2"}," Hazel as typed glue between heterogeneous GAIOS tools",-1)),s(` <div class="grid grid-cols-2 gap-2">
+  <img src="/images/tldraw-petrinaut.png" style="width: 100%; border: 1px solid #ddd;" />
+  <img src="/images/shadow.png" style="width: 100%; border: 1px solid #ddd;" />
+</div> `),s(` <div class="text-sm mt-2 opacity-70">
+Sketch a flowchart, get a Petri net. Write code that transforms a drawing. David will show you more.
+</div> `)]),_:1},16))}};export{C as default};
